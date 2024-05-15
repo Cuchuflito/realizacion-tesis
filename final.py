aff78fd6c9785984482bf84ebb6eca165715d3ab
Cuchuflito <111323461+Cuchuflito@users.noreply.github.com>
2024-05-15 17:53:38 -0400

    se cambia el deshacer
    se arregla deshacer

--- final.py
+++ final.py
@@ -152,25 +152,34 @@ class ImageSegmentationApp:
     def save_to_historia(self): #se guarda el estado de la imagen en la pila de historial
         current_state = {
             'displayed_image': self.displayed_image.copy(),
-            'labels': self.labels[:],
-            'polygon_points': self.polygon_points[:]
-        }
+            'labels': list(self.labels), #se copian las etiquetas (labels) y los puntos del polígono
+            'polygon_points': list(self.polygon_points)
+            }
         self.historia.append(current_state)
 
-    def undo_last_action(self): #se deshace la última acción realizada llamando al estado anterior almacenado en la pila de historial (historia)
+    def clear_current_canvas(self):
+        self.canvas.delete("all") #se limpia el canvas actual
+        
+    def undo_last_action(self):
         if self.historia:
+            self.clear_current_canvas()  # Limpiar el canvas para evitar superposiciones visuales
+            # Restaurar el último estado guardado
             state = self.historia.pop()
-            self.displayed_image = state['displayed_image']
-            self.labels = state['labels']
-            self.polygon_points = state['polygon_points']
-            if self.is_drawing_polygon:
-                if self.current_polygon:
-                    self.canvas.delete(self.current_polygon)
+            self.displayed_image = np.copy(state['displayed_image'])
+            self.labels = list(state['labels'])
+            self.polygon_points = list(state['polygon_points'])
+            self.is_drawing_polygon = state.get('is_drawing_polygon', False)
+            
+            # Si había un polígono siendo dibujado, eliminarlo visualmente
+            if self.current_polygon:
+                self.canvas.delete(self.current_polygon)
                 self.current_polygon = None
-                self.is_drawing_polygon = False
+
+            # Actualizar la visualización con el estado restaurado
             self.show_segmented_image()
         else:
             print("No hay más acciones para deshacer.")
+
 
     def apply_kmeans(self): #aplicar k-means a la imagen según el imput que diga el usuario
         self.save_to_historia()
@@ -234,8 +243,8 @@ class ImageSegmentationApp:
             self.start_drag(event)
 
     def paint_segment(self, x, y, color): #aplicar floodfill a la imagen (herramienta bote de pintura)
-        tolerance = 20
         self.save_to_historia()
+        tolerance = 20
         mask = np.zeros((self.painted_image.shape[0] + 2, self.painted_image.shape[1] + 2), np.uint8)
         cv2.floodFill(self.painted_image, mask, (x, y), color, (tolerance,) * 3, (tolerance,) * 3, flags=cv2.FLOODFILL_FIXED_RANGE)
         self.displayed_image = self.painted_image.copy()
@@ -268,10 +277,124 @@ class ImageSegmentationApp:
             self.show_segmented_image()
 
     def finish_polygon(self): #finalizar el etiquetado de un polígono pintado. El nombre elegido queda en medio y se guarda en la pila de historial
+        self.save_to_historia()
         if self.is_drawing_polygon and self.polygon_points:
             label = simpledialog.askstring("Etiqueta", "Introduce el nombre del sector:")
             if label:
-                self.save_to_historia()
+                centroid_x, centroid_y = self.safe_polygon_centroid(self.polygon_points)
+                scaled_centroid_x = int((centroid_x - self.offset_x) / self.scale)
+                scaled_centroid_y = int((centroid_y - self.offset_y) / self.scale)
+                self.labels.append((label, (scaled_centroid_x, scaled_centroid_y)))
+                self.show_segmented_image()
+                self.is_drawing_polygon = False
+                self.polygon_points = []
+            if self.current_polygon:
+                self.canvas.delete(self.current_polygon)
+                self.current_polygon = None
+        else:
+            print("No hay más acciones para deshacer.")
+
+
+    def apply_kmeans(self): #aplicar k-means a la imagen según el imput que diga el usuario
+        self.save_to_historia()
+        k = int(self.k_entry.get())
+        kmeans = KMeans(n_clusters=k, random_state=0)
+        data = self.original_image.reshape((-1, 3)).astype(np.float32)
+        kmeans.fit(data)
+        self.segmented_image = kmeans.cluster_centers_[kmeans.labels_].reshape(self.original_image.shape).astype(np.uint8)
+        self.current_image = self.segmented_image.copy()
+        self.painted_image = self.segmented_image.copy()
+        self.displayed_image = self.painted_image.copy()
+        self.show_segmented_image()
+
+    def show_segmented_image(self): #muestra la imagen segmentada por k-means
+        resized_image = cv2.resize(self.displayed_image, None, fx=self.scale, fy=self.scale, interpolation=cv2.INTER_LINEAR)
+        img = Image.fromarray(resized_image)
+        draw = ImageDraw.Draw(img)
+        draw.font = self.font
+
+        for label, (center_x, center_y) in self.labels:
+            screen_x = int(center_x * self.scale)
+            screen_y = int(center_y * self.scale)
+
+            box_width, box_height = draw.font.getbbox(label, anchor='lt')[2:]
+            box_x = screen_x - box_width // 2
+            box_y = screen_y - box_height // 2
+            draw.rectangle([box_x - 2, box_y - 2, box_x + box_width + 2, box_y + box_height + 2], fill='black')
+            draw.text((box_x, box_y), label, fill='white', font=self.font)
+
+        self.photo_image = ImageTk.PhotoImage(image=img)
+        self.canvas.config(width=self.canvas_width * self.scale, height=self.canvas_height * self.scale)
+        self.canvas.create_image(self.offset_x, self.offset_y, image=self.photo_image, anchor=NW)
+        
+        
+    def safe_polygon_centroid(self, points):         #Encuentra un punto seguro dentro del polígono usando shapely.
+        poly = Polygon(points)
+        point = poly.representative_point()  # Devuelve un punto garantizado dentro del polígono
+        return point.x, point.y
+
+
+    def handle_click(self, event): #manejo de clicks. Implementación de lógica entre lazo, pintar y arrastrar
+        mode = self.mode_var.get()
+        if mode == "lazo":
+            if not self.is_drawing_polygon:
+                self.polygon_points = [(event.x, event.y)]
+                self.is_drawing_polygon = True
+                if self.current_polygon:
+                    self.canvas.delete(self.current_polygon)
+                self.current_polygon = self.canvas.create_polygon(self.polygon_points, outline='red', fill='', width=2)
+            else:
+                self.polygon_points.append((event.x, event.y))
+                self.canvas.coords(self.current_polygon, sum(self.polygon_points, ()))
+        elif mode == "paint":
+            color_map = {"red": (255, 0, 0), "blue": (0, 0, 255), "green": (0, 255, 0), "yellow": (255, 255, 0)}
+            chosen_color = color_map[self.color_var.get()]
+            original_x = int((event.x - self.offset_x) / self.scale)
+            original_y = int((event.y - self.offset_y) / self.scale)
+            if 0 <= original_x < self.painted_image.shape[1] and 0 <= original_y < self.painted_image.shape[0]:
+                self.paint_segment(original_x, original_y, chosen_color)
+        elif mode == "drag":
+            self.start_drag(event)
+
+    def paint_segment(self, x, y, color): #aplicar floodfill a la imagen (herramienta bote de pintura)
+        self.save_to_historia()
+        tolerance = 20
+        mask = np.zeros((self.painted_image.shape[0] + 2, self.painted_image.shape[1] + 2), np.uint8)
+        cv2.floodFill(self.painted_image, mask, (x, y), color, (tolerance,) * 3, (tolerance,) * 3, flags=cv2.FLOODFILL_FIXED_RANGE)
+        self.displayed_image = self.painted_image.copy()
+        self.show_segmented_image()
+
+    def start_drag(self, event): #manejo de arrastre
+        self.drag_start_x = event.x
+        self.drag_start_y = event.y
+
+    def drag(self, event): #arrastrar la imagen
+        if hasattr(self, 'drag_start_x') and self.drag_start_x is not None:
+            self.offset_x += event.x - self.drag_start_x
+            self.offset_y += event.y - self.drag_start_y
+            self.drag_start_x = event.x
+            self.drag_start_y = event.y
+            self.show_segmented_image()
+
+    def reset_drag(self, event): #luego de arrastrada la imagen, se resetea el arrastre
+        self.drag_start_x = None
+        self.drag_start_y = None
+
+    def zoom_in(self):
+        if self.scale < 5:
+            self.scale *= 1.1
+            self.show_segmented_image()
+
+    def zoom_out(self):
+        if self.scale > 0.5:
+            self.scale *= 0.9
+            self.show_segmented_image()
+
+    def finish_polygon(self): #finalizar el etiquetado de un polígono pintado. El nombre elegido queda en medio y se guarda en la pila de historial
+        self.save_to_historia()
+        if self.is_drawing_polygon and self.polygon_points:
+            label = simpledialog.askstring("Etiqueta", "Introduce el nombre del sector:")
+            if label:
                 centroid_x, centroid_y = self.safe_polygon_centroid(self.polygon_points)
                 scaled_centroid_x = int((centroid_x - self.offset_x) / self.scale)
                 scaled_centroid_y = int((centroid_y - self.offset_y) / self.scale)
